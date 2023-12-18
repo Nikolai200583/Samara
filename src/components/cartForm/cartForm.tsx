9f53a381c3ae058811c6a62ef4bee2cf1bf5970b
Nikolai <bonum_trd@bk.ru>
2023-12-18 16:30:33 +0500

    333

--- src/components/cartForm/cartForm.tsx
+++ src/components/cartForm/cartForm.tsx
@@ -1,5 +1,5 @@
-import { useForm } from "react-hook-form";
-import React, { SetStateAction, Dispatch } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { SetStateAction, Dispatch} from "react";
 
 interface MyForm {
   firstName: string;
@@ -18,11 +18,12 @@ interface cartProps {
   count: number;
 }
 
-export const CartForm = ({ isModal, setCount, count }: cartProps) => {
+export const CartForm = ({ isModal, setUser, setCount, count }: cartProps) => {
   const {
-  
-    formState: { isValid },
-   
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    reset
   } = useForm<MyForm>({
     mode: "onBlur"
   });
@@ -32,15 +33,15 @@ export const CartForm = ({ isModal, setCount, count }: cartProps) => {
     isModal(true);
   }
 
-  // const onSubmit: SubmitHandler<MyForm> = (data) => {
+  const onSubmit: SubmitHandler<MyForm> = (data) => {
 
-  //   // const mail = `mailto:${data.mail}?subject=`
-  //   // const header = encodeURIComponent(`"Тестовое задание, заказ №${count}"`)
-  //   // const body = encodeURIComponent(`"${data.firstName}, заказ №${count} сформирован. В ближайшее время наш специалист свяжется с вами по телефону ${data.phone}"`);
-  //   // window.open(mail + header + "&body=" + body);
-  //   setUser(data);
-  //   reset();
-  // };
+    const mail = `mailto:${data.mail}?subject=`
+    const header = encodeURIComponent(`"Тестовое задание, заказ №${count}"`)
+    const body = encodeURIComponent(`"${data.firstName}, заказ №${count} сформирован. В ближайшее время наш специалист свяжется с вами по телефону ${data.phone}"`);
+    window.open(mail + header + "&body=" + body);
+    setUser(data);
+    reset();
+  };
 
   return (
     <section className="section-formAction">
@@ -48,70 +49,70 @@ export const CartForm = ({ isModal, setCount, count }: cartProps) => {
         <h3 className="cart__form_title">Пожалуйста, представьтесь</h3>
       
         <form
-          action="sendmail.php"
-          method="POST"
-          // onSubmit={handleSubmit(onSubmit)}
+          action="/index.html"
+          method="post"
+          onSubmit={handleSubmit(onSubmit)}
           className="cart__form_input"
         >
           <div>
             <input
-              // {...register("firstName", {
-              //   required: "Поле обязательно к заполнению",
-              //   minLength: {
-              //     value: 3,
-              //     message: "Минимум 3 значения"
-              //   }
-              // })}
+              {...register("firstName", {
+                required: "Поле обязательно к заполнению",
+                minLength: {
+                  value: 3,
+                  message: "Минимум 3 значения"
+                }
+              })}
               placeholder="Ваше имя"
               type="text"
               className="cart__form_input-name"
             />
-            {/* <div className="errorMessage">
+            <div className="errorMessage">
               {errors?.firstName && <p>{errors?.firstName?.message}</p>}
-            </div> */}
+            </div>
           </div>
           <div>
             <input
-              // {...register("phone", {
-              //   required: "Поле обязательно к заполнению",
-              //   pattern: {
-              //     value:
-              //       /(\+)?((\d{2,3}) ?\d|\d)(([ -]?\d)|( ?(\d{2,3}) ?)){5,12}\d/,
-              //     message: "Не корректный номер телефона"
-              //   }
-              // })}
+              {...register("phone", {
+                required: "Поле обязательно к заполнению",
+                pattern: {
+                  value:
+                    /(\+)?((\d{2,3}) ?\d|\d)(([ -]?\d)|( ?(\d{2,3}) ?)){5,12}\d/,
+                  message: "Не корректный номер телефона"
+                }
+              })}
               placeholder="Телефон"
               type="tel"
               className="cart__form_input-telephone"
             />
-            {/* <div className="errorMessage">
+            <div className="errorMessage">
               {errors?.phone && <p>{errors?.phone?.message}</p>}
-            </div> */}
+            </div>
           </div>
           <div>
             <input
-              // {...register("mail", {
-              //   required: "Поле обязательно к заполнению",
-              //   pattern: {
-              //     value: /[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+/,
-              //     message: "Не корректный адрес электронной почты"
-              //   }
-              // })}
+              {...register("mail", {
+                required: "Поле обязательно к заполнению",
+                pattern: {
+                  value: /[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+/,
+                  message: "Не корректный адрес электронной почты"
+                }
+              })}
               placeholder="Email"
               type="text"
               className="cart__form_input-adress"
             />
-            {/* <div className="errorMessage">
+            <div className="errorMessage">
               {" "}
               {errors?.mail && <p>{errors?.mail?.message}</p>}
-            </div> */}
+            </div>
           </div>
 
           <button
             placeholder="Оформить заказ"
             className="cart__form_input-button"
-            // type="submit"
-            // disabled={!isValid}
+            type="submit"
+            disabled={!isValid}
             onClick={counter}
             style={{
               backgroundColor: isValid ? "#296DC1" : "rgba(41, 109, 193, 0.5)"
